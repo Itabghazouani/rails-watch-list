2aafcb55856536cd5c93c7e401b157436edd2527
itab ghazouani <itab.ghazouani@gmail.com>
2022-07-13 13:03:27 +0200

    Still missing image_tags and few issues, need help for this

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,40 @@
+class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
+
+  # GET /lists/:list_id/bookmarks/new
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  # POST /lists/:list_id/bookmarks
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = @bookmark.list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /bookmarks/:id
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+end
